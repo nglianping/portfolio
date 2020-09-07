a564c60667ac171331bb72ee9ba96b77b52b4f2a
ping94 <ngping94@gmail.com>
2020-09-07 21:49:23 +0800

    Version 1.2 Portfolio
    Update responsive section

---
+++ script.js
@@ -0,0 +1,41 @@
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// fade_in
+$(document).ready(function () {
+  $(window).scroll(function () {
+    $(".uni_details").each(function (i) {
+      var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      if (bottom_of_window > bottom_of_element) {
+        $(this).animate({ opacity: "1" }, 1000);
+      }
+    });
+
+    $(".company_details").each(function (i) {
+      var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      if (bottom_of_window > bottom_of_element) {
+        $(this).animate({ opacity: "1" }, 1000);
+      }
+    });
+  });
+});
